2e84cc109b85457a5cd7aa96e4575ddfd487112a
wrightL <anon2pai@gmail.com>
2024-12-04 16:22:55 +0700

    Update functor.js

--- functor.js
+++ functor.js
@@ -1,3 +1,4 @@
+const { HttpsProxyAgent } = require('https-proxy-agent');
 const {
   Worker,
   isMainThread,
@@ -6,10 +7,6 @@ const {
 } = require("worker_threads");
 const fs = require("fs");
 const axios = require("axios");
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 const randomUserAgent = require("random-useragent");
 
 function clearConsole() {
@@ -42,69 +39,61 @@ function readAccounts() {
   return accounts;
 }
 
-async function claimMiningReward(userId, headers, email) {
+async function claimMiningReward(userId, headers, email, axiosConfig) {
   try {
-    await axios.get(`https://apix.securitylabs.xyz/v1/users/earn/${userId}`, {
-      headers
-    });
+    await axios.get(`https://apix.securitylabs.xyz/v1/users/earn/${userId}`, axiosConfig);
     parentPort.postMessage(`\x1b[38;5;121mReward mining diklaim untuk user ${maskEmail(email)}\x1b[0m`);
   } catch (error) {
     parentPort.postMessage(
-      `\x1b[38;5;168mKesalahan klaim reward mining : ${error.response.data.message}\x1b[0m`
+      `\x1b[38;5;168mKesalahan klaim reward mining : ${error.response?.data?.message || error.message}\x1b[0m`
     );
   }
 }
 
-async function activateEpoch(headers) {
+async function activateEpoch(headers, axiosConfig) {
   try {
-    await axios.get("https://apix.securitylabs.xyz/v1/epoch/active", {
-      headers: {
-        ...headers,
-        Origin: "chrome-extension://gahmmgacnfeohncipkjfjfbdlpbfkfhi"
-      }
-    });
+    const activateHeaders = { ...headers };
+    activateHeaders.Origin = "chrome-extension://gahmmgacnfeohncipkjfjfbdlpbfkfhi";
+
+    await axios.get("https://apix.securitylabs.xyz/v1/epoch/active", { ...axiosConfig, headers: activateHeaders });
     parentPort.postMessage(`\x1b[38;5;121mEpoch diaktifkan\x1b[0m`);
   } catch (error) {
     parentPort.postMessage(`\x1b[38;5;168mKesalahan aktivasi epoch: ${error.message}\x1b[0m`);
   }
 }
 
-async function activateEpochLoop(headers) {
+async function activateEpochLoop(headers, axiosConfig) {
   try {
-    await activateEpoch(headers);
+    await activateEpoch(headers, axiosConfig);
     const randomDelay = Math.floor(Math.random() * 3 + 1) * 60000;
     parentPort.postMessage(
       `\x1b[38;5;147mMenunggu ${randomDelay / 60000} menit sebelum aktivasi epoch lagi\x1b[0m`
     );
-    setTimeout(() => activateEpochLoop(headers), randomDelay);
+    setTimeout(() => activateEpochLoop(headers, axiosConfig), randomDelay);
   } catch (error) {
     parentPort.postMessage(
       `\x1b[38;5;168mKesalahan pada activateEpochLoop: ${error.message}\x1b[0m`
     );
-    setTimeout(() => activateEpochLoop(headers), 60000);
+    setTimeout(() => activateEpochLoop(headers, axiosConfig), 60000);
   }
 }
-
-const headers = {
-  Host: "apix.securitylabs.xyz",
-  "Sec-Ch-Ua": '"Chromium";v="123", "Not:A-Brand";v="8"',
-  Accept: "application/json, text/plain, */*",
-  "Sec-Ch-Ua-Mobile": "?0",
-  "User-Agent": randomUserAgent.getRandom(),
-  "Sec-Ch-Ua-Platform": "Linux",
-  Origin: "https://node.securitylabs.xyz",
-  "Sec-Fetch-Site": "same-site",
-  "Sec-Fetch-Mode": "cors",
-  "Sec-Fetch-Dest": "empty",
-  Referer: "https://node.securitylabs.xyz/",
-  "Accept-Encoding": "gzip, deflate, br",
-  "Accept-Language": "en-US,en;q=0.9",
-  Connection: "close"
-};
 
 async function workerMain() {
   const { email, password } = workerData;
   const maskedEmail = maskEmail(email);
+  let proxyString = null;
+  let proxyStatus = "Tanpa Proxy";
+
+  try {
+    const proxyFileContent = fs.existsSync("proxy.txt") ? fs.readFileSync("proxy.txt", "utf-8") : "";
+    if (proxyFileContent.trim() !== "") {
+      proxyString = proxyFileContent.trim().split("\n")[0];
+    }
+  } catch (err) {
+    console.error(`\x1b[38;5;168m[${maskedEmail}] Gagal membaca file proxy: ${err.message}\x1b[0m`);
+  }
+
+
   const headers = {
     Host: "apix.securitylabs.xyz",
     "Sec-Ch-Ua": '"Chromium";v="123", "Not:A-Brand";v="8"',
@@ -123,13 +112,30 @@ async function workerMain() {
     "Content-Type": "application/json"
   };
 
+
+  let agent = null;
+  let axiosConfig = { headers };
+
+  if (proxyString) {
+    try {
+      agent = new HttpsProxyAgent(proxyString);
+      proxyStatus = `Proxy: ${proxyString.split('@')[1] || proxyString}`;
+      axiosConfig.httpsAgent = agent;
+      axiosConfig.proxy = false;
+    } catch (err) {
+      console.error(`\x1b[38;5;168m[${maskedEmail}] Proxy tidak valid: ${err.message}\x1b[0m`);
+    }
+  }
+
   try {
-    parentPort.postMessage(`\x1b[38;5;122mMemproses akun: ${maskedEmail}\x1b[0m`);
+    parentPort.postMessage(`[${maskEmail(email)}] Memproses akun: ${maskedEmail}`);
+
     const checkEmailResponse = await axios.post(
       "https://apix.securitylabs.xyz/v1/auth/check-exist-email",
       { email },
-      { headers }
+      axiosConfig
     );
+
     if (!checkEmailResponse.data) {
       parentPort.postMessage(`\x1b[38;5;168mEmail tidak ditemukan untuk akun: ${maskedEmail}\x1b[0m`);
       return;
@@ -138,37 +144,44 @@ async function workerMain() {
     const signInResponse = await axios.post(
       "https://apix.securitylabs.xyz/v1/auth/signin-user",
       { email, password },
-      { headers }
+      axiosConfig
     );
     const accessToken = signInResponse.data.accessToken;
 
     headers["Authorization"] = `Bearer ${accessToken}`;
+    axiosConfig.headers = headers;
 
-    const getUserResponse = await axios.get("https://apix.securitylabs.xyz/v1/users", { headers });
+
+    const getUserResponse = await axios.get("https://apix.securitylabs.xyz/v1/users", axiosConfig);
     const userId = getUserResponse.data.id;
 
-    const getWalletResponse = await axios.get("https://apix.securitylabs.xyz/v1/wallets", { headers });
+    const getWalletResponse = await axios.get("https://apix.securitylabs.xyz/v1/wallets", axiosConfig);
     const maskedWallet = maskWalletAddress(getWalletResponse.data[0].address);
-    parentPort.postMessage(`\x1b[38;5;123mAlamat wallet: ${maskedWallet}\x1b[0m`);
+    parentPort.postMessage(`\x1b[38;5;123m[${maskEmail(email)}] Alamat wallet: ${maskedWallet}\x1b[0m`);
 
-    claimMiningReward(userId, headers, email);
-    setInterval(() => claimMiningReward(userId, headers, email), 86400000);
+    claimMiningReward(userId, headers, email, axiosConfig);
+    setInterval(() => claimMiningReward(userId, headers, email, axiosConfig), 86400000);
 
-    activateEpochLoop(headers);
+    activateEpochLoop(headers, axiosConfig);
+
+    parentPort.postMessage(`[${maskEmail(email)}] Berhasil Login Dengan Email: ${maskedEmail} | ${proxyStatus}`);
+
   } catch (error) {
-    parentPort.postMessage(`\x1b[38;5;168mKesalahan untuk akun ${maskedEmail}: ${error.message}\x1b[0m`);
+    parentPort.postMessage(`\x1b[38;5;168m[${maskedEmail}] Kesalahan untuk akun ${maskedEmail}: ${error.message}\x1b[0m`);
   }
 }
 
+
 if (isMainThread) {
   clearConsole();
+
   function main() {
     const accounts = readAccounts();
     for (const account of accounts) {
       const worker = new Worker(__filename, { workerData: account });
 
       worker.on("message", (message) => {
-        console.log(`\x1b[38;5;116m[${maskEmail(account.email)}] ${message}\x1b[0m`);
+        console.log(`\x1b[38;5;116m${message}\x1b[0m`);
       });
 
       worker.on("error", (error) => {
